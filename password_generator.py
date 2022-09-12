e76f1c64913789073f6585908b12098f8606f25e
adeland <100533215+adeland@users.noreply.github.com>
2022-09-12 15:18:29 -0400

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,14 +1,17 @@
+from lib2to3.pygram import Symbols
 import string
 from random import choice, shuffle
+from symtable import Symbol
 
 def randomPass():
     # Possible values for password
     lowercase = list(string.ascii_lowercase)
     uppercase = list(string.ascii_uppercase)
     numbers = list(string.digits)
+    symbol = list(r'\/:*?"<>-@#$^&|') 
 
     # Combination of mixed values
-    combinatrix = uppercase + lowercase + numbers
+    combinatrix = uppercase + lowercase + numbers + symbol
     shuffle(combinatrix)
 
     # User input for password length
